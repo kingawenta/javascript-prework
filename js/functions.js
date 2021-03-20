1f6b0644414745abc44fdb9ed8de08d215889e05
Kinga Wenta <kinga.wenta@gmail.com>
2021-03-21 00:52:15 +0100

    New version

--- js/functions.js
+++ js/functions.js
@@ -21,6 +21,26 @@ if (randomNumber == '3') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+var playerInput, playerMove;
+playerInput = 0;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  printMessage('błędna odpowiedź');
+}
+if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  printMessage('błędna odpowiedź');
+}
+printMessage('Twój ruch: ' + playerMove);
 }
 
 function clearMessages(){
