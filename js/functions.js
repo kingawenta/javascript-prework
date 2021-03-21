71fba16ffed3d02cafb6da09dfc07324ec192e83
Kinga Wenta <kinga.wenta@gmail.com>
2021-03-21 17:15:51 +0100

    Add js

--- js/functions.js
+++ js/functions.js
@@ -2,45 +2,6 @@ function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
-	var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
-}
-if (randomNumber == '2') {
-  computerMove = 'papier';
-} else {
-  computerMove = 'nieznany ruch';
-}
-if (randomNumber == '3') {
-  computerMove = 'nożyce';
-} else {
-  computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-var playerInput, playerMove;
-playerInput = 0;
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else {
-  playerMove = 'nieznany ruch';
-}
-if (playerInput == '2') {
-  playerMove = 'papier';
-} else {
-  printMessage('błędna odpowiedź');
-}
-if (playerInput == '3') {
-  playerMove = 'nożyce';
-} else {
-  printMessage('błędna odpowiedź');
-}
-printMessage('Twój ruch: ' + playerMove);
 }
 
 function clearMessages(){
